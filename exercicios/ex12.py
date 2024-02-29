df5e47654cefc8d1bd1639cbf385fe435d325bb9
KandidoBNunes <135878286+KandidoBNunes@users.noreply.github.com>
2024-02-29 14:59:04 -0300

    Add files via upload

---
+++ exercicios/ex12.py
@@ -0,0 +1,6 @@
+num1 = int(input('me diga um numero:'))
+num05 = num1 / 2
+num2 = num1 * 2
+num3 = num1 * 3
+num4 = num1 ** (1/2)
+print('a metade seria {} o dobro seria {} o triplo seria {} e a raiz seria{}'.format(num05, num2, num3, num4)) 
---
+++ exercicios/ex17.py
@@ -0,0 +1,5 @@
+lar = float(input('qual a largura da parede?? '))
+alt = float(input('qual a altura da parede?? '))
+tinta = alt * lar
+tintat = tinta / 2
+print('voce vai precisar do equivalente a {} metros quadrados em tinta o que equivale a {} litros'.format(tinta, tintat))
