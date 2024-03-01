82dc7e9ceb054d5d5945d2eb08df1ce5a57fe42b
KandidoBNunes <135878286+KandidoBNunes@users.noreply.github.com>
2024-03-01 16:25:04 -0300

    Add files via upload

---
+++ exercicios/ex25.py
@@ -0,0 +1,12 @@
+import random
+
+ops1 = input(' me diga a uma das opcoes: ')
+ops2 = input(' me diga a uma das opcoes: ')
+ops3 = input(' me diga a uma das opcoes: ')
+ops4 = input(' me diga a uma das opcoes: ')
+ops5 = input(' me diga a uma das opcoes: ')
+
+opsf = [ops1 , ops2 , ops3 , ops4 , ops5]
+
+sorteio = random.choice(opsf)
+print('o escolhido é {} '.format(sorteio))
