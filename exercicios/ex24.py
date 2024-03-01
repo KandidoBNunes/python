2ebe736eeef8bdc511bff4fd39c808d1489ade34
KandidoBNunes <135878286+KandidoBNunes@users.noreply.github.com>
2024-03-01 16:24:45 -0300

    Add files via upload

---
+++ exercicios/ex24.py
@@ -0,0 +1,7 @@
+import math
+ang = float(input('digite o angulo em graus: '))
+angrad = math.radians(ang)
+
+print('seno',math.sin(angrad))
+print('cosseno', math.cos(angrad))
+print('target', math.tan(angrad))
