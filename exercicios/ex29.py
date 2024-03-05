bd8ef5afd547e6218510e947b6b6f3817072df4f
KandidoBNunes <135878286+KandidoBNunes@users.noreply.github.com>
2024-03-05 14:12:24 -0300

    Add files via upload

---
+++ exercicios/ex29.py
@@ -0,0 +1,10 @@
+num= input('digite qualquer numero inteiro de 0 a 9999:')
+num0 = num[0]
+num1 = num[1]
+num2 = num[2]
+num3 = num[3]
+print("""esse numero tem 
+      {} milhares
+      {} centenas
+      {} dezenas
+      {} unidaddes""".format(num0,num1,num2,num3))
