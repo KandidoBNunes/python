ec8ad20f6dec7fda04e42849d4ac49034d899b58
KandidoBNunes <135878286+KandidoBNunes@users.noreply.github.com>
2024-03-11 15:08:27 -0300

    Add files via upload

---
+++ exercicios/ex47.py
@@ -0,0 +1,7 @@
+nota =float(input('me diga sua nota'))
+if nota <= 5:
+    print('REPROVADO')
+elif nota >5 < 6.9:
+    print('RECUPERACAO')
+elif nota > 7 :
+    print('APROVADO')
---
+++ exercicios/ex48.py
@@ -0,0 +1,11 @@
+idade = int(input('me diga sua idade: '))
+if idade < 9:
+    print('mirim')
+elif idade <9>14:
+    print('infantil')
+elif idade <14>18:
+    print('junior')
+elif idade <18>20:
+    print('senior')
+else:
+    print('master')
