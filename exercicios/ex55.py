93e7955c3ed14c31d421b4a6c06c8012b9d16062
KandidoBNunes <135878286+KandidoBNunes@users.noreply.github.com>
2024-03-20 15:45:26 -0300

    Add files via upload

---
+++ exercicios/ex55.py
@@ -0,0 +1,13 @@
+sit = input('estado de emprego: ')
+if sit == "desempregado ":
+    print('fudido')
+elif sit == "estudante":
+    print('fudido premium')
+elif sit == "clt":
+    print('fudido porem a salariado')
+elif sit == 'pj' or 'MEI':
+    print('fudido e emprendedor')
+elif sit == "aposentado":
+    print('sortudo')
+else:
+    print('agora n sei mais...')
